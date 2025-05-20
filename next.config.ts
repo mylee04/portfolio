a721cc7862485a85483684d1221007249f840cf5
Myungeun Lee <mylee@deloitte.com>
2025-05-20 15:15:43 -0400

    update next.js(ts here) config for Github Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: { unoptimized: true },
+  // GitHub Actions가 자동으로 basePath를 설정하므로 수동으로 추가할 필요 없음
 };
 
 export default nextConfig;
