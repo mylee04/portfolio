d83590846c627d9f7bae63a46758c31a752ef129
Myungeun Lee <mylee@deloitte.com>
2025-05-20 15:29:38 -0400

    fix images error

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   output: 'export',
   images: { unoptimized: true },
   // GitHub Actions가 자동으로 basePath를 설정하므로 수동으로 추가할 필요 없음
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
 };
 
 export default nextConfig;
